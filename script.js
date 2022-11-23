dd2aa459611fc46d0e5ad10d7edee5c308297602
Tarun Kumar <116190775+tarunkumarkushwaha@users.noreply.github.com>
2022-11-23 08:27:07 +0000

    am/pm + 12 hour clock addition

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 const myclock = () => {
-  let t = new Date()
-  let h = t.getHours()
-  let m = t.getMinutes()
-  let s = t.getSeconds()
-  ghari.innerHTML = " Hamar ghari  " + h + ":" + m + ":" + s
+  let t = new Date();
+  let h = (12 - (t.getHours()));
+  let m = t.getMinutes();
+  let s = t.getSeconds();
+  let ampm = (t.getHours()) < 12 ? 'AM' : 'PM';
+  if (h < 0) {
+    h = h * -1;
+  } else if (h == 00) {
+    h = 12;
+  } else {
+    h = h;
+  }
+  ghari.innerHTML = " Hamar ghari  " + addZero(h) + ":" + addZero(m) + ":" + addZero(s) + "" + ampm;
 }
-  setInterval(myclock, 1000)
+const addZero = (time) => {
+  return (time < 10) ? "0" + time : time;
+}
+setInterval(myclock, 1000)
+
+
+
